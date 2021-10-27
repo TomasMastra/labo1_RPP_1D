f5fdbbdbdb039597ce1e3c946aa6cd4a7a2cefc1
unknown <tomas26013@gmail.com>
2021-10-27 13:08:27 -0300

    parte 1-2 correccion fechas

--- MastrapasquaTomas_RPP/parte1-2/src/Fechas.c
+++ MastrapasquaTomas_RPP/parte1-2/src/Fechas.c
@@ -26,7 +26,7 @@ void pedirFecha(eEstadia estadia[], int i)
 
 	 getInt(&estadia[i].fecha.dia, "Ingrese el dia (1-31):\n","Error, ingrese el dia (1-31):\n", 1, 31);
 	getInt(&estadia[i].fecha.mes, "Ingrese el mes (1-12):\n","Error, ingrese el mes (1-12):\n", 1, 12);
-	getInt(&estadia[i].fecha.anio, "Ingrese el año (2000 - 2021):\n","Error, ingrese el año (2000 - 2021):\n", 2000, 2021);
+	getInt(&estadia[i].fecha.anio, "Ingrese el año (2021 - 2030):\n","Error, ingrese el año (2021 - 2030):\n", 2021, 2030);
 
 }
 
